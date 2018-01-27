1092c1952ea354c94de3a34937357d6071e6bf17
meatyminx <flexayoung@gmail.com>
2018-01-27 15:53:25 +0100

    Fyrsta commit, smávægilegar breytingar

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
+//Exportum router fyrir articles.js
+const router = require('./articles');
 
-const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/grein', router);
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
+//const articles = require("./articles");
+
+//
+
+
+// const marked = require('marked');
+// const fs = require('fs');
+// const fm = require('front-matter');
--- articles.js
+++ articles.js
@@ -1,1 +1,15 @@
 /* útfæra greina virkni */
+const express = require('express');
+const router = express.Router();
+
+router.get('/:name', (req, res) => {
+    res.end(req.params.name);
+    res.redirect('/halleluja');
+});
+
+
+module.exports = router;
+
+/* app.get('route', cb1, cb2, cb3..) {
+
+} */
